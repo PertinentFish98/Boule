a6f8662d20312c089ddeeb1c89b0af5d21200bd8
PertinentFish98 <140010643+PertinentFish98@users.noreply.github.com>
2023-11-08 21:39:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,29 @@ document.querySelector('#btnIndex').addEventListener('click', function () {
 
 const bpm = document.getElementById('bpm');
 const notes = document.getElementById('notes');
-var newNotes = [...notes.value.split('-')];
 
-const buttonBpm = document.querySelector('#btnBpm');
-const buttonNotes = document.querySelector('#btnNotes');
+const newNotes = [...notes.value.split('-')];
 
-const dataToSend = JSON.stringify(bpm.value, newNotes);
+const A = document.querySelector('#btn1').value;
+const Ap = document.querySelector('#btn2').value;
+const B = document.querySelector('#btn3').value;
+const C = document.querySelector('#btn4').value;
+const Cp = document.querySelector('#btn5').value;
+const D = document.querySelector('#btn6').value;
+const Dp = document.querySelector('#btn7').value;
+const E = document.querySelector('#btn8').value;
+const F = document.querySelector('#btn9').value;
+const Fp = document.querySelector('#btn10').value;
+const G = document.querySelector('#btn11').value;
+const Gp = document.querySelector('#btn12').value;
+
+const Notes = [A, Ap, B, C, Cp, D, Dp, E, F, Fp, G, Gp];
+
+const dataToSend = JSON.stringify(bpm.value, newNotes, Notes);
 let dataReceived = '';
 
 fetch('', {
-  credentials: 'http://172.24.1.1/metalo/',
+  credentials: 'http://172.24.1.1',
   mode: '172.24.1.1',
   method: 'get',
   headers: { 'Content-Type': 'application/json' },
@@ -41,9 +54,3 @@ fetch('', {
   });
 
 console.log(`Received:${dataReceived}`);
-
-let i;
-
-for (i = 0; i < newNotes.length; i++) {
-  $.get(`http://172.24.1.1/metalo/note${newNotes[i]}.cgi`);
-}
